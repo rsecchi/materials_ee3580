fc85a403b6442d9005fdf6e590336d57ca12e31b
Raffaello <raffaello@erg.abdn.ac.uk>
2020-11-05 14:08:43 +0000

    Pushing modifications

--- misc_verilog/dpll.v
+++ misc_verilog/dpll.v
@@ -1,45 +1,71 @@
-
+`timescale 1ns/1ns
 
 module dpll(clk, rst, in);
-input wire clk;
-input wire rst;
-input wire in;
+input wire clk;       // clk
+input wire rst;       // reset wire
+input wire in;        // input wave
 	
-reg [15:0] phase;
-
+reg [7:0] phase;
+//reg [7:0] count;
 
 	always @(posedge clk) 
 	begin
 
 		if (rst == 1'b1)
-			phase <= 16'h00;
+			begin
+			// reset counters
+				phase <= 8'h00;
+//				count <= 8'h00;
+			end
 		else
-			phase <= phase + 1;
-		end 
-		
+			begin
+				// update input counter and phase
+				// phase <= phase + 16'h6590;
+				//count <= count + phase[15] ^ in;
+				phase <= phase + 16'h6590 + phase[15] ^ in;
+			end
 	end
 
 endmodule
 
 
+// This is the testbench module
 module test;
-	
-	reg clock;
-	always #5000 clock = ~clock;   // 100 kHZ clock
 
-	reg in = 0;
+
+	integer Fclk = 1e6;       // clock frequency 
+
+	
+	// generating a clock waveform at 100kHz
+	reg clock;
+	initial
+	begin 
+		clock = 0;
+		forever #5000 clock = ~clock;   // 100 kHZ clock
+	end
+
+	// generating  an input at 40kHz, phase = +144 degs
+	reg in;
+	initial
+	begin 
+		#10000 in = 0;
+		forever #12500 in = ~in;   // 40 kHZ input wave
+	end
+
 	reg rst;
-	dpll xorpll(.clk=clock, .rst=rst, 
+	dpll test_dpll(.clk(clock), .rst(rst), .in(in)); 
 
 	initial
 	begin
-		$dumpfile("dpll.vcc");
+		$display("frequency %d\n", f0);
+		$dumpfile("dpll.vcd");
 		$dumpvars(0, test);
 
 		#1 rst <= 1'b0;
-		#10000 rst <= 1'b1;
+		#20000 rst <= 1'b1;
+		#11000 rst <= 1'b0;
 
-		#100000 $finish;
+		#200000 $finish;
 		
 	end
 
